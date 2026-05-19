5fedab134b280acec6bfef0eac866ae4c8770d52
maxjgarcia <garcia.maximiliano.jesus@gmail.com>
2026-05-20 06:26:34 +0800

    first commit

---
+++ BackEnd.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class BackEnd {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int value = scanner.nextInt() ;
+
+        System.out.println(value);
+
+        scanner.close();
+    }
+}
