123f8354f2ce3b11530031a23e13d0b98eb3e968
maxjgarcia <garcia.maximiliano.jesus@gmail.com>
2026-05-21 23:24:57 +0800

    primer menu agregado

--- com/backend/productos/Productos.java
+++ com/backend/productos/Productos.java
@@ -3,11 +3,32 @@ import java.util.Scanner;
 
 public class Productos {
     public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in);
-        char value = scanner.next().charAt(0);
-        
 
-        System.out.println(value);
+        Scanner scanner = new Scanner(System.in);
+        
+        int option = 0;
+
+        while(option != 4){
+            System.out.println("ingresar una opcion");
+            option = scanner.nextInt();
+
+           switch (option) {
+            case 1:
+                System.out.println("1");
+                break;
+            case 2:
+                System.out.println("2");
+                break;
+            case 3:
+                System.out.println("3");
+                break;
+            case 4:
+                break;
+            default:
+                break;
+        } 
+        }
+
 
         scanner.close();
     }
